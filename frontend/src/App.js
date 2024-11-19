6507c6284a7a24666bd3e511828b3124bf209c77
Chu Huy Quang <quangdtm2004@gmail.com>
2024-11-19 23:33:55 +0700

    reconfig frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-8 rounded-lg">
+      <h1 className="text-3xl font-bold">Hello, TailwindCSS with React!</h1>
+    </div>
+  );
+}
+
+export default App;
