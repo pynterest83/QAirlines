5b514c23952969fb85d501148bb4da172d630725
VaguePasta <qphuc21158@gmail.com>
2024-11-20 08:41:50 +0700

    Regenerate react with vite

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="bg-blue-500 text-white text-center p-8 rounded-lg">
-      <h1 className="text-3xl font-bold">Hello, TailwindCSS with React!</h1>
-    </div>
-  );
-}
-
-export default App;
